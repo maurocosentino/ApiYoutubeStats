5226962b40308efc14b80b0259d0d68c71a23fa4
maurocosentino <mauroxcosentino@gmail.com>
2025-07-06 00:09:48 -0300

    Add files via upload

---
+++ DTOs/YouTubePlaylistDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiYoutubeStats.DTOs
+{
+    public class YouTubePlaylistDto
+    {
+        public string Id { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Author { get; set; } = "";
+        public string Url { get; set; } = "";
+        public List<YouTubeSearchResultDto> Videos { get; set; } = new();
+    }
+
+}
